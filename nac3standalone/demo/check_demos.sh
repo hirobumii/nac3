f9aceb24eee0c0bac54e21bdf331880ad192d3ce
David Mak <csdavidmak@ust.hk>
2023-09-29 15:17:45 +0800

    standalone: Split check_demos into check_demo
    Allows individual tests to be executed.

--- nac3standalone/demo/check_demos.sh
+++ nac3standalone/demo/check_demos.sh
@@ -4,12 +4,8 @@ set -e
 
 count=0
 for demo in src/*.py; do
-    echo -n "checking $demo... "
-    ./interpret_demo.py $demo > interpreted.log
-    ./run_demo.sh "$@" $demo > run.log
-    diff -Nau interpreted.log run.log
-    echo "ok"
-    let "count+=1"
+  ./check_demo.sh "$@" "$demo"
+  ((count += 1))
 done
 
 echo "Ran $count demo checks - PASSED"
