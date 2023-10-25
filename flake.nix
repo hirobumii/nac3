ea2ab0ef7c9fbaf7d57b5f7f2986f474b20c98e3
Sebastien Bourdeauducq <sb@m-labs.hk>
2023-10-25 21:09:22 +0800

    update nixpkgs, python 3.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The third-generation ARTIQ compiler";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/master;
 
   outputs = { self, nixpkgs }:
     let
