bb5147521f612561ad695a8578bc4bd851ea872b
David Mak <csdavidmak@ust.hk>
2023-10-30 17:19:18 +0800

    standalone: Fix indentation of test files

--- nac3standalone/demo/src/list_slice_issue315.py
+++ nac3standalone/demo/src/list_slice_issue315.py
@@ -1,17 +1,17 @@
 @extern
 def output_int32(x: int32):
-	...
+    ...
 
 @extern
 def output_int32_list(x: list[int32]):
-	...
+    ...
 
 def run() -> int32:
-	bl = [True, False]
+    bl = [True, False]
 
-	bl1 = bl[:]
-	bl1[1:] = [True]
-	output_int32_list([int32(b) for b in bl1])
-	output_int32_list([int32(b) for b in bl1])
+    bl1 = bl[:]
+    bl1[1:] = [True]
+    output_int32_list([int32(b) for b in bl1])
+    output_int32_list([int32(b) for b in bl1])
 
-	return 0
+    return 0
