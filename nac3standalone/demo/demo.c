bb27e3d4004e956f843aacff5d49bf93e3278f38
David Mak <csdavidmak@ust.hk>
2023-10-30 17:22:32 +0800

    standalone: Fix indentation of demo.c

--- nac3standalone/demo/demo.c
+++ nac3standalone/demo/demo.c
@@ -5,91 +5,91 @@
 #include <string.h>
 
 #if __SIZEOF_POINTER__ == 8
-  #define usize uint64_t
+    #define usize uint64_t
 #elif __SIZEOF_POINTER__ == 4
-  #define usize uint32_t
+    #define usize uint32_t
 #else
-  #error "Unsupported platform - Platform is not 32-bit or 64-bit"
+    #error "Unsupported platform - Platform is not 32-bit or 64-bit"
 #endif
 
 void output_bool(bool x) {
-  puts(x ? "True" : "False");
+    puts(x ? "True" : "False");
 }
 
 void output_int32(int32_t x) {
-  printf("%d\n", x);
+    printf("%d\n", x);
 }
 
 void output_int64(int64_t x) {
-  printf("%ld\n", x);
+    printf("%ld\n", x);
 }
 
 void output_uint32(uint32_t x) {
-  printf("%d\n", x);
+    printf("%d\n", x);
 }
 
 void output_uint64(uint64_t x) {
-  printf("%ld\n", x);
+    printf("%ld\n", x);
 }
 
 void output_float64(double x) {
-  printf("%f\n", x);
+    printf("%f\n", x);
 }
 
 void output_asciiart(int32_t x) {
-  static const char *chars = " .,-:;i+hHM$*#@  ";
-  if (x < 0) {
-    putchar('\n');
-  } else {
-    putchar(chars[x]);
-  }
+    static const char *chars = " .,-:;i+hHM$*#@    ";
+    if (x < 0) {
+        putchar('\n');
+    } else {
+        putchar(chars[x]);
+    }
 }
 
 struct cslice {
-  void *data;
-  usize len;
+    void *data;
+    usize len;
 };
 
 void output_int32_list(struct cslice *slice) {
-  const int32_t *data = (int32_t *) slice->data;
+    const int32_t *data = (int32_t *) slice->data;
 
-  putchar('[');
-  for (usize i = 0; i < slice->len; ++i) {
-    if (i == slice->len - 1) {
-      printf("%d", data[i]);
-    } else {
-      printf("%d, ", data[i]);
+    putchar('[');
+    for (usize i = 0; i < slice->len; ++i) {
+        if (i == slice->len - 1) {
+            printf("%d", data[i]);
+        } else {
+            printf("%d, ", data[i]);
+        }
     }
-  }
-  putchar(']');
-  putchar('\n');
+    putchar(']');
+    putchar('\n');
 }
 
 void output_str(struct cslice *slice) {
-  const char *data = (const char *) slice->data;
+    const char *data = (const char *) slice->data;
 
-  for (usize i = 0; i < slice->len; ++i) {
-    putchar(data[i]);
-  }
-  putchar('\n');
+    for (usize i = 0; i < slice->len; ++i) {
+        putchar(data[i]);
+    }
+    putchar('\n');
 }
 
 uint64_t dbg_stack_address(__attribute__((unused)) struct cslice *slice) {
-  int i;
-  void *ptr = (void *) &i;
-  return (uintptr_t) ptr;
+    int i;
+    void *ptr = (void *) &i;
+    return (uintptr_t) ptr;
 }
 
 uint32_t __nac3_personality(uint32_t state, uint32_t exception_object, uint32_t context) {
-  printf("__nac3_personality(state: %u, exception_object: %u, context: %u\n", state, exception_object, context);
-  exit(101);
-  __builtin_unreachable();
+    printf("__nac3_personality(state: %u, exception_object: %u, context: %u\n", state, exception_object, context);
+    exit(101);
+    __builtin_unreachable();
 }
 
 uint32_t __nac3_raise(uint32_t state, uint32_t exception_object, uint32_t context) {
-  printf("__nac3_raise(state: %u, exception_object: %u, context: %u\n", state, exception_object, context);
-  exit(101);
-  __builtin_unreachable();
+    printf("__nac3_raise(state: %u, exception_object: %u, context: %u\n", state, exception_object, context);
+    exit(101);
+    __builtin_unreachable();
 }
 
 void __nac3_end_catch(void) {}
@@ -97,5 +97,5 @@ void __nac3_end_catch(void) {}
 extern int32_t run(void);
 
 int main(void) {
-  run();
+    run();
 }
