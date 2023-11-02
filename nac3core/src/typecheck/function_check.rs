029dc6f61df030a85023ad10a18b20e5472631f0
David Mak <csdavidmak@ust.hk>
2023-11-02 16:04:55 +0800

    core: Add compile-time error and runtime assertion for negative shifts

--- nac3core/src/typecheck/function_check.rs
+++ nac3core/src/typecheck/function_check.rs
@@ -2,7 +2,7 @@ use crate::typecheck::typedef::TypeEnum;
 
 use super::type_inferencer::Inferencer;
 use super::typedef::Type;
-use nac3parser::ast::{self, Expr, ExprKind, Stmt, StmtKind, StrRef};
+use nac3parser::ast::{self, Constant, Expr, ExprKind, Operator::{LShift, RShift}, Stmt, StmtKind, StrRef};
 use std::{collections::HashSet, iter::once};
 
 impl<'a> Inferencer<'a> {
@@ -107,11 +107,28 @@ impl<'a> Inferencer<'a> {
                 self.check_expr(value, defined_identifiers)?;
                 self.should_have_value(value)?;
             }
-            ExprKind::BinOp { left, right, .. } => {
+            ExprKind::BinOp { left, op, right } => {
                 self.check_expr(left, defined_identifiers)?;
                 self.check_expr(right, defined_identifiers)?;
                 self.should_have_value(left)?;
                 self.should_have_value(right)?;
+
+                // Check whether a bitwise shift has a negative RHS constant value
+                if *op == LShift || *op == RShift {
+                    if let ExprKind::Constant { value, .. } = &right.node {
+                        let rhs_val = match value {
+                            Constant::Int(v) => v,
+                            _ => unreachable!(),
+                        };
+
+                        if *rhs_val < 0 {
+                            return Err(format!(
+                                "shift count is negative at {}",
+                                right.location
+                            ));
+                        }
+                    }
+                }
             }
             ExprKind::UnaryOp { operand, .. } => {
                 self.check_expr(operand, defined_identifiers)?;
