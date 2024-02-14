282a3e1911bf767060de1c12a3363dd6e985e933
David Mak <chmakac@connect.ust.hk>
2024-02-14 16:18:11 +0800

    core: Fix typo in error message

--- nac3core/src/typecheck/function_check.rs
+++ nac3core/src/typecheck/function_check.rs
@@ -337,7 +337,7 @@ impl<'a> Inferencer<'a> {
                         if !self.check_return_value_ty(ret_ty) {
                             return Err(HashSet::from([
                                 format!(
-                                    "return value of type {} must be a primitive of a tuple of primitives at {}",
+                                    "return value of type {} must be a primitive or a tuple of primitives at {}",
                                     self.unifier.stringify(ret_ty),
                                     value.location,
                                 ),
