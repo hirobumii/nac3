e1dbe2526a8fbc7367ef44c480e0e544f0a328bc
Sebastien Bourdeauducq <sb@m-labs.hk>
2024-02-20 15:46:51 +0800

    flake: switch to nixpkgs unstable for newer rustc

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The third-generation ARTIQ compiler";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
   outputs = { self, nixpkgs }:
     let
