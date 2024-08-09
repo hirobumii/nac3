1ccfad9c531c217884d198b5662acc7f0fd4e889
David Mak <chmakac@connect.ust.hk>
2024-08-09 12:26:59 +0800

    standalone: Fix several issues post script refactoring
    - Add helptext for check_demos.sh
    - Add back support for using debug NAC3 for running tests
    - Output error message when argument is not recognized
    - Fixed last non-demo script argument being ignored
    - Add back SSE2 requirement to NAC3 (required for mandelbrot)

--- nac3standalone/demo/check_demo.sh
+++ nac3standalone/demo/check_demo.sh
@@ -8,11 +8,14 @@ if [ -z "$1" ]; then
 fi
 
 declare -a nac3args
-while [ $# -ge 2 ]; do
+while [ $# -gt 1 ]; do
   case "$1" in
     --help)
-      echo "Usage: check_demo.sh [-i686] -- demo [NAC3ARGS...]"
+      echo "Usage: check_demo.sh [--debug] [-i686] -- [NAC3ARGS...] demo"
       exit
+      ;;
+    --debug)
+      debug=1
       ;;
     -i686)
       i686=1
@@ -22,18 +25,18 @@ while [ $# -ge 2 ]; do
       break
       ;;
     *)
-      break
+      echo "Unrecognized argument \"$1\""
+      exit 1
       ;;
   esac
   shift
 done
 
-demo="$1"
-shift
 while [ $# -gt 1 ]; do
   nac3args+=("$1")
   shift
 done
+demo="$1"
 
 
 echo "### Checking $demo..."
@@ -43,12 +46,20 @@ echo ">>>>>> Running $demo with the Python interpreter"
 
 if [ -n "$i686" ]; then
   echo "...... Trying NAC3's 32-bit code generator output"
-  ./run_demo.sh -i686 --out run_32.log "${nac3args[@]}" "$demo"
+  if [ -n "$debug" ]; then
+    ./run_demo.sh --debug -i686 --out run_32.log -- "${nac3args[@]}" "$demo"
+  else
+    ./run_demo.sh -i686 --out run_32.log -- "${nac3args[@]}" "$demo"
+  fi
   diff -Nau interpreted.log run_32.log
 fi
 
 echo "...... Trying NAC3's 64-bit code generator output"
-./run_demo.sh --out run_64.log "${nac3args[@]}" "$demo"
+if [ -n "$debug" ]; then
+  ./run_demo.sh --debug --out run_64.log -- "${nac3args[@]}" "$demo"
+else
+  ./run_demo.sh --out run_64.log -- "${nac3args[@]}" "$demo"
+fi
 diff -Nau interpreted.log run_64.log
 
 echo "...... OK"
