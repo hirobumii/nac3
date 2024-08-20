fa047d505d6c6bc7f25bad74bfe57d7e9b829c3f
lyken <lyken@m-labs.hk>
2024-08-20 15:17:10 +0800

    core/ndstrides: implement np_identity() and np_eye()

--- nac3core/src/codegen/object/ndarray/factory.rs
+++ nac3core/src/codegen/object/ndarray/factory.rs
@@ -1,4 +1,4 @@
-use inkwell::values::BasicValueEnum;
+use inkwell::{values::BasicValueEnum, IntPredicate};
 
 use super::NDArrayObject;
 use crate::{
@@ -121,5 +121,55 @@ impl<'ctx> NDArrayObject<'ctx> {
     ) -> Self {
         let fill_value = ndarray_one_value(generator, ctx, dtype);
         NDArrayObject::make_np_full(generator, ctx, dtype, ndims, shape, fill_value)
+    }
+
+    /// Create an ndarray like `np.eye`.
+    pub fn make_np_eye<G: CodeGenerator + ?Sized>(
+        generator: &mut G,
+        ctx: &mut CodeGenContext<'ctx, '_>,
+        dtype: Type,
+        nrows: Instance<'ctx, Int<SizeT>>,
+        ncols: Instance<'ctx, Int<SizeT>>,
+        offset: Instance<'ctx, Int<SizeT>>,
+    ) -> Self {
+        let ndzero = ndarray_zero_value(generator, ctx, dtype);
+        let ndone = ndarray_one_value(generator, ctx, dtype);
+
+        let ndarray = NDArrayObject::alloca_dynamic_shape(generator, ctx, dtype, &[nrows, ncols]);
+
+        // Create data and make the matrix like look np.eye()
+        ndarray.create_data(generator, ctx);
+        ndarray
+            .foreach(generator, ctx, |generator, ctx, _hooks, nditer| {
+                // NOTE: rows and cols can never be zero here, since this ndarray's `np.size` would be zero
+                // and this loop would not execute.
+
+                // Load up `row_i` and `col_i` from indices.
+                let row_i = nditer.get_indices().get_index_const(generator, ctx, 0);
+                let col_i = nditer.get_indices().get_index_const(generator, ctx, 1);
+
+                let be_one = row_i.add(ctx, offset).compare(ctx, IntPredicate::EQ, col_i);
+                let value = ctx.builder.build_select(be_one.value, ndone, ndzero, "value").unwrap();
+
+                let p = nditer.get_pointer(generator, ctx);
+                ctx.builder.build_store(p, value).unwrap();
+
+                Ok(())
+            })
+            .unwrap();
+
+        ndarray
+    }
+
+    /// Create an ndarray like `np.identity`.
+    pub fn make_np_identity<G: CodeGenerator + ?Sized>(
+        generator: &mut G,
+        ctx: &mut CodeGenContext<'ctx, '_>,
+        dtype: Type,
+        size: Instance<'ctx, Int<SizeT>>,
+    ) -> Self {
+        // Convenient implementation
+        let offset = Int(SizeT).const_0(generator, ctx.ctx);
+        NDArrayObject::make_np_eye(generator, ctx, dtype, size, size, offset)
     }
 }
