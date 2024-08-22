54e56172705ef26b0b143f3ca8e089fd161040e6
lyken <lyken@m-labs.hk>
2024-08-22 16:21:01 +0800

    core: remove old ndarray code and NDArray proxy
    Nothing depends on the old ndarray implementation now.

--- nac3core/src/codegen/test.rs
+++ nac3core/src/codegen/test.rs
@@ -1,6 +1,6 @@
 use crate::{
     codegen::{
-        classes::{ListType, NDArrayType, ProxyType, RangeType},
+        classes::{ListType, ProxyType, RangeType},
         concrete_type::ConcreteTypeStore,
         CodeGenContext, CodeGenLLVMOptions, CodeGenTargetMachineOptions, CodeGenTask,
         CodeGenerator, DefaultCodeGenerator, WithCall, WorkerRegistry,
@@ -455,16 +455,4 @@ fn test_classes_range_type_new() {
 
     let llvm_range = RangeType::new(&ctx);
     assert!(RangeType::is_type(llvm_range.as_base_type()).is_ok());
-}
-
-#[test]
-fn test_classes_ndarray_type_new() {
-    let ctx = inkwell::context::Context::create();
-    let generator = DefaultCodeGenerator::new(String::new(), 64);
-
-    let llvm_i32 = ctx.i32_type();
-    let llvm_usize = generator.get_size_type(&ctx);
-
-    let llvm_ndarray = NDArrayType::new(&generator, &ctx, llvm_i32.into());
-    assert!(NDArrayType::is_type(llvm_ndarray.as_base_type(), llvm_usize).is_ok());
 }
