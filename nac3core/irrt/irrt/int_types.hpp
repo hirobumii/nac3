bd06155f348ecb421ad88fca65405f3a49408570
Sébastien Bourdeauducq <sb@m-labs.hk>
2024-09-05 18:54:55 +0800

    irrt: compatibility with pre-C23 compilers

--- nac3core/irrt/irrt/int_types.hpp
+++ nac3core/irrt/irrt/int_types.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
+#if __STDC_VERSION__ >= 202000
 using int8_t = _BitInt(8);
 using uint8_t = unsigned _BitInt(8);
 using int32_t = _BitInt(32);
 using uint32_t = unsigned _BitInt(32);
 using int64_t = _BitInt(64);
 using uint64_t = unsigned _BitInt(64);
+#else
+using int8_t = _ExtInt(8);
+using uint8_t = unsigned _ExtInt(8);
+using int32_t = _ExtInt(32);
+using uint32_t = unsigned _ExtInt(32);
+using int64_t = _ExtInt(64);
+using uint64_t = unsigned _ExtInt(64);
+#endif
 
 // NDArray indices are always `uint32_t`.
 using NDIndex = uint32_t;
