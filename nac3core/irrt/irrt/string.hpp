9b0d37b1f0198347ab55204f8b6084ae386cd6c4
ram <RAMTEJ001@e.ntu.edu.sg>
2024-12-12 10:29:00 +0000

    Amend to follow formatting of other C++ files

--- nac3core/irrt/irrt/string.hpp
+++ nac3core/irrt/irrt/string.hpp
@@ -13,11 +13,11 @@ SizeT __nac3_str_eq_impl(const char* str1, SizeT len1, const char* str2, SizeT l
 }  // namespace
 
 extern "C" {
-int64_t nac3_str_eq(const char* str1, uint64_t len1, const char* str2, uint64_t len2) {
-    return __nac3_str_eq_impl<uint64_t>(str1, len1, str2, len2);
+uint32_t nac3_str_eq(const char* str1, uint32_t len1, const char* str2, uint32_t len2) {
+    return __nac3_str_eq_impl<uint32_t>(str1, len1, str2, len2);
 }
 
-int32_t nac3_str_eq_i32(const char* str1, uint32_t len1, const char* str2, uint32_t len2) {
-    return __nac3_str_eq_impl<uint32_t>(str1, len1, str2, len2);
+uint64_t nac3_str_eq64(const char* str1, uint64_t len1, const char* str2, uint64_t len2) {
+    return __nac3_str_eq_impl<uint64_t>(str1, len1, str2, len2);
 }
 }
