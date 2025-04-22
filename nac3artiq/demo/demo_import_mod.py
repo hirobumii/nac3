8031e9143c531f3a6ce7176bb01127785274cde0
David Mak <chmakac@connect.ust.hk>
2025-04-22 13:41:49 +0800

    [artiq] Use `import module as` in demo_import_mod.py

--- nac3artiq/demo/demo_import_mod.py
+++ nac3artiq/demo/demo_import_mod.py
@@ -1,26 +1,26 @@
 from min_artiq import kernel, KernelInvariant, nac3
-import min_artiq
+import min_artiq as artiq
 
 
 @nac3
 class Demo:
-    core: KernelInvariant[min_artiq.Core]
-    led0: KernelInvariant[min_artiq.TTLOut]
-    led1: KernelInvariant[min_artiq.TTLOut]
+    core: KernelInvariant[artiq.Core]
+    led0: KernelInvariant[artiq.TTLOut]
+    led1: KernelInvariant[artiq.TTLOut]
 
     def __init__(self):
-        self.core = min_artiq.Core()
-        self.led0 = min_artiq.TTLOut(self.core, 18)
-        self.led1 = min_artiq.TTLOut(self.core, 19)
+        self.core = artiq.Core()
+        self.led0 = artiq.TTLOut(self.core, 18)
+        self.led1 = artiq.TTLOut(self.core, 19)
 
     @kernel
     def run(self):
         self.core.reset()
         while True:
-            with min_artiq.parallel:
-                self.led0.pulse(100.*min_artiq.ms)
-                self.led1.pulse(100.*min_artiq.ms)
-            self.core.delay(100.*min_artiq.ms)
+            with artiq.parallel:
+                self.led0.pulse(100.*artiq.ms)
+                self.led1.pulse(100.*artiq.ms)
+            self.core.delay(100.*artiq.ms)
 
 
 if __name__ == "__main__":
