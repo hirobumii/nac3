11b422c8dd1a694d740e4bcb2ce027e16b912c97
ivan <th@m-labs.hk>
2025-06-17 12:25:06 +0800

    simplify intrinsics

--- nac3core/src/codegen/llvm_intrinsics.rs
+++ nac3core/src/codegen/llvm_intrinsics.rs
@@ -1,99 +1,84 @@
 use inkwell::{
     AddressSpace,
     intrinsics::Intrinsic,
-    types::AnyTypeEnum::IntType,
-    values::{BasicValueEnum, CallSiteValue, FloatValue, IntValue, PointerValue},
+    types::{AnyTypeEnum::IntType, BasicTypeEnum},
+    values::{BasicMetadataValueEnum, BasicValueEnum, FloatValue, IntValue, PointerValue},
 };
-use itertools::Either;
 
 use super::CodeGenContext;
 
-/// Invokes the [`llvm.va_start`](https://llvm.org/docs/LangRef.html#llvm-va-start-intrinsic)
-/// intrinsic.
+fn _call_intrinsic<'ctx>(
+    ctx: &CodeGenContext<'ctx, '_>,
+    intrin: &str,
+    // These are *type parameters* for overloaded functions (the ".i8" part of
+    // "llvm.ctpop.i8"), not the types of the parameters of the function!
+    type_params: &[BasicTypeEnum<'ctx>],
+    args: &[BasicMetadataValueEnum<'ctx>],
+    call_name: Option<&str>,
+) -> Option<BasicValueEnum<'ctx>> {
+    let intrin = Intrinsic::find(intrin)
+        .and_then(|intrinsic| intrinsic.get_declaration(&ctx.module, type_params))
+        .expect("intrinsic not found");
+    let result = ctx.builder.build_call(intrin, args, call_name.unwrap_or_default()).unwrap();
+    result.try_as_basic_value().left()
+}
+
+macro_rules! call_intrinsic {
+    ($ctx: expr, $call_name: expr, $intrin:literal $([$($type_param:expr),*])? ($($arg:expr),*)) => {{
+        _call_intrinsic($ctx, concat!("llvm.", $intrin), &[$($($type_param.into()),*)?], &[$($arg.into()),*], $call_name)
+    }};
+    ($ctx: expr, $call_name: expr, $intrin:literal $([$($type_param:expr),*])? ($($arg:expr),*) -> void) => {{
+        assert!(call_intrinsic!($ctx, $call_name, $intrin $([$($type_param),*])? ($($arg),*)).is_none())
+    }};
+    ($ctx: expr, $call_name: expr, $intrin:literal $([$($type_param:expr),*])? ($($arg:expr),*) -> int) => {{
+        call_intrinsic!($ctx, $call_name, $intrin $([$($type_param),*])? ($($arg),*)).unwrap().into_int_value()
+    }};
+    ($ctx: expr, $call_name: expr, $intrin:literal $([$($type_param:expr),*])? ($($arg:expr),*) -> float) => {{
+        call_intrinsic!($ctx, $call_name, $intrin $([$($type_param),*])? ($($arg),*)).unwrap().into_float_value()
+    }};
+    ($ctx: expr, $call_name: expr, $intrin:literal $([$($type_param:expr),*])? ($($arg:expr),*) -> ptr) => {{
+        call_intrinsic!($ctx, $call_name, $intrin $([$($type_param),*])? ($($arg),*)).unwrap().into_pointer_value()
+    }};
+}
+
+macro_rules! llvm_doc {
+    ($llvm_name:literal) => {
+        concat!(
+            "Invokes the [`llvm.",
+            $llvm_name,
+            "`](https://llvm.org/docs/LangRef.html#llvm-",
+            $llvm_name,
+            "-intrinsic) intrinsic."
+        )
+    };
+}
+
+#[doc = llvm_doc!("va_start")]
 pub fn call_va_start<'ctx>(ctx: &CodeGenContext<'ctx, '_>, arglist: PointerValue<'ctx>) {
-    const FN_NAME: &str = "llvm.va_start";
-
-    let intrinsic_fn = ctx.module.get_function(FN_NAME).unwrap_or_else(|| {
-        let llvm_void = ctx.ctx.void_type();
-        let llvm_i8 = ctx.ctx.i8_type();
-        let llvm_p0i8 = llvm_i8.ptr_type(AddressSpace::default());
-        let fn_type = llvm_void.fn_type(&[llvm_p0i8.into()], false);
-
-        ctx.module.add_function(FN_NAME, fn_type, None)
-    });
-
-    ctx.builder.build_call(intrinsic_fn, &[arglist.into()], "").unwrap();
+    call_intrinsic!(ctx, None, "va_start"(arglist) -> void)
 }
 
-/// Invokes the [`llvm.va_end`](https://llvm.org/docs/LangRef.html#llvm-va-end-intrinsic)
-/// intrinsic.
+#[doc = llvm_doc!("va_end")]
 pub fn call_va_end<'ctx>(ctx: &CodeGenContext<'ctx, '_>, arglist: PointerValue<'ctx>) {
-    const FN_NAME: &str = "llvm.va_end";
-
-    let intrinsic_fn = ctx.module.get_function(FN_NAME).unwrap_or_else(|| {
-        let llvm_void = ctx.ctx.void_type();
-        let llvm_i8 = ctx.ctx.i8_type();
-        let llvm_p0i8 = llvm_i8.ptr_type(AddressSpace::default());
-        let fn_type = llvm_void.fn_type(&[llvm_p0i8.into()], false);
-
-        ctx.module.add_function(FN_NAME, fn_type, None)
-    });
-
-    ctx.builder.build_call(intrinsic_fn, &[arglist.into()], "").unwrap();
+    call_intrinsic!(ctx, None, "va_end"(arglist) -> void)
 }
 
-/// Invokes the [`llvm.stacksave`](https://llvm.org/docs/LangRef.html#llvm-stacksave-intrinsic)
-/// intrinsic.
+#[doc = llvm_doc!("va_stacksave")]
 pub fn call_stacksave<'ctx>(
     ctx: &CodeGenContext<'ctx, '_>,
     name: Option<&str>,
 ) -> PointerValue<'ctx> {
-    const FN_NAME: &str = "llvm.stacksave";
-
-    let intrinsic_fn = Intrinsic::find(FN_NAME)
-        .and_then(|intrinsic| intrinsic.get_declaration(&ctx.module, &[]))
-        .unwrap();
-
-    ctx.builder
-        .build_call(intrinsic_fn, &[], name.unwrap_or_default())
-        .map(CallSiteValue::try_as_basic_value)
-        .map(|v| v.map_left(BasicValueEnum::into_pointer_value))
-        .map(Either::unwrap_left)
-        .unwrap()
+    call_intrinsic!(ctx, name, "stacksave"() -> ptr)
 }
 
-/// Invokes the
-/// [`llvm.stackrestore`](https://llvm.org/docs/LangRef.html#llvm-stackrestore-intrinsic) intrinsic.
+#[doc = llvm_doc!("va_stackrestore")]
 ///
 /// - `ptr`: The pointer storing the address to restore the stack to.
 pub fn call_stackrestore<'ctx>(ctx: &CodeGenContext<'ctx, '_>, ptr: PointerValue<'ctx>) {
-    const FN_NAME: &str = "llvm.stackrestore";
-
-    /*
-        SEE https://github.com/TheDan64/inkwell/issues/496
-
-        We want `llvm.stackrestore`, but the following would generate `llvm.stackrestore.p0i8`.
-        ```ignore
-        let intrinsic_fn = Intrinsic::find(FN_NAME)
-            .and_then(|intrinsic| intrinsic.get_declaration(&ctx.module, &[llvm_p0i8.into()]))
-            .unwrap();
-        ```
-
-        Temp workaround by manually declaring the intrinsic with the correct function name instead.
-    */
-    let intrinsic_fn = ctx.module.get_function(FN_NAME).unwrap_or_else(|| {
-        let llvm_void = ctx.ctx.void_type();
-        let llvm_i8 = ctx.ctx.i8_type();
-        let llvm_p0i8 = llvm_i8.ptr_type(AddressSpace::default());
-        let fn_type = llvm_void.fn_type(&[llvm_p0i8.into()], false);
-
-        ctx.module.add_function(FN_NAME, fn_type, None)
-    });
-
-    ctx.builder.build_call(intrinsic_fn, &[ptr.into()], "").unwrap();
+    call_intrinsic!(ctx, None, "stackrestore"(ptr) -> void)
 }
 
-/// Invokes the [`llvm.memcpy`](https://llvm.org/docs/LangRef.html#llvm-memcpy-intrinsic) intrinsic.
+#[doc = llvm_doc!("memcpy")]
 ///
 /// * `dest` - The pointer to the destination. Must be a pointer to an integer type.
 /// * `src` - The pointer to the source. Must be a pointer to an integer type.
@@ -123,7 +108,7 @@ pub fn call_memcpy<'ctx>(
     ctx.builder.build_memcpy(dest, dest_alignment, src, src_alignment, len).unwrap();
 }
 
-/// Invokes the `llvm.memcpy` intrinsic.
+#[doc = llvm_doc!("memcpy")]
 ///
 /// Unlike [`call_memcpy`], this function accepts any type of pointer value. If `dest` or `src` is
 /// not a pointer to an integer, the pointer(s) will be cast to `i8*` before invoking `memcpy`.
@@ -159,7 +144,7 @@ pub fn call_memcpy_generic<'ctx>(
     call_memcpy(ctx, dest, src, len);
 }
 
-/// Invokes the `llvm.memcpy` intrinsic.
+#[doc = llvm_doc!("memcpy")]
 ///
 /// Unlike [`call_memcpy`], this function accepts any type of pointer value. If `dest` or `src` is
 /// not a pointer to an integer, the pointer(s) will be cast to `i8*` before invoking `memcpy`.
@@ -204,25 +189,6 @@ pub fn call_memcpy_generic_array<'ctx>(
     call_memcpy(ctx, dest, src, len);
 }
 
-/// Macro to find and generate build call for llvm intrinsic (body of llvm intrinsic function)
-///
-/// Arguments:
-/// * `$ctx:ident`: Reference to the current Code Generation Context
-/// * `$name:ident`: Optional name to be assigned to the llvm build call (Option<&str>)
-/// * `$llvm_name:literal`: Name of underlying llvm intrinsic function
-/// * `$map_fn:ident`: Mapping function to be applied on `BasicValue` (`BasicValue` -> Function Return Type).
-///   Use `BasicValueEnum::into_int_value` for Integer return type and
-///   `BasicValueEnum::into_float_value` for Float return type
-/// * `$llvm_ty:ident`: Type of first operand
-/// * `,($val:ident)*`: Comma separated list of operands
-macro_rules! generate_llvm_intrinsic_fn_body {
-    ($ctx:ident, $name:ident, $llvm_name:literal, $map_fn:expr, $llvm_ty:ident $(,$val:ident)*) => {{
-        const FN_NAME: &str = concat!("llvm.", $llvm_name);
-        let intrinsic_fn = Intrinsic::find(FN_NAME).and_then(|intrinsic| intrinsic.get_declaration(&$ctx.module, &[$llvm_ty.into()])).unwrap();
-        $ctx.builder.build_call(intrinsic_fn, &[$($val.into()),*],  $name.unwrap_or_default()).map(CallSiteValue::try_as_basic_value).map(|v| v.map_left($map_fn)).map(Either::unwrap_left).unwrap()
-    }};
-}
-
 /// Macro to generate the llvm intrinsic function using [`generate_llvm_intrinsic_fn_body`].
 ///
 /// Arguments:
@@ -233,19 +199,18 @@ macro_rules! generate_llvm_intrinsic_fn_body {
 /// * `$val:ident`: The operand for unary operations
 /// * `$val1:ident`, `$val2:ident`: The operands for binary operations
 macro_rules! generate_llvm_intrinsic_fn {
-    ("float", $fn_name:ident, $llvm_name:literal, $val:ident) => {
-        #[doc = concat!("Invokes the [`", stringify!($llvm_name), "`](https://llvm.org/docs/LangRef.html#llvm-", stringify!($llvm_name), "-intrinsic) intrinsic." )]
+    (float $fn_name:ident: $llvm_name:literal ($val:ident)) => {
+        #[doc = llvm_doc!($llvm_name)]
         pub fn $fn_name<'ctx> (
             ctx: &CodeGenContext<'ctx, '_>,
             $val: FloatValue<'ctx>,
             name: Option<&str>,
         ) -> FloatValue<'ctx> {
-            let llvm_ty = $val.get_type();
-            generate_llvm_intrinsic_fn_body!(ctx, name, $llvm_name, BasicValueEnum::into_float_value, llvm_ty, $val)
+            call_intrinsic!(ctx, name, $llvm_name[$val.get_type()]($val) -> float)
         }
     };
-    ("float", $fn_name:ident, $llvm_name:literal, $val1:ident, $val2:ident) => {
-        #[doc = concat!("Invokes the [`", stringify!($llvm_name), "`](https://llvm.org/docs/LangRef.html#llvm-", stringify!($llvm_name), "-intrinsic) intrinsic." )]
+    (float $fn_name:ident: $llvm_name:literal ($val1:ident, $val2:ident)) => {
+        #[doc = llvm_doc!($llvm_name)]
         pub fn $fn_name<'ctx> (
             ctx: &CodeGenContext<'ctx, '_>,
             $val1: FloatValue<'ctx>,
@@ -253,12 +218,11 @@ macro_rules! generate_llvm_intrinsic_fn {
             name: Option<&str>,
         ) -> FloatValue<'ctx> {
             debug_assert_eq!($val1.get_type(), $val2.get_type());
-            let llvm_ty = $val1.get_type();
-            generate_llvm_intrinsic_fn_body!(ctx, name, $llvm_name, BasicValueEnum::into_float_value, llvm_ty, $val1, $val2)
+            call_intrinsic!(ctx, name, $llvm_name[$val1.get_type()]($val1, $val2) -> float)
         }
     };
-    ("int", $fn_name:ident, $llvm_name:literal, $val1:ident, $val2:ident) => {
-        #[doc = concat!("Invokes the [`", stringify!($llvm_name), "`](https://llvm.org/docs/LangRef.html#llvm-", stringify!($llvm_name), "-intrinsic) intrinsic." )]
+    (int $fn_name:ident: $llvm_name:literal ($val1:ident, $val2:ident)) => {
+        #[doc = llvm_doc!($llvm_name)]
         pub fn $fn_name<'ctx> (
             ctx: &CodeGenContext<'ctx, '_>,
             $val1: IntValue<'ctx>,
@@ -266,13 +230,12 @@ macro_rules! generate_llvm_intrinsic_fn {
             name: Option<&str>,
         ) -> IntValue<'ctx> {
             debug_assert_eq!($val1.get_type().get_bit_width(), $val2.get_type().get_bit_width());
-            let llvm_ty = $val1.get_type();
-            generate_llvm_intrinsic_fn_body!(ctx, name, $llvm_name, BasicValueEnum::into_int_value, llvm_ty, $val1, $val2)
+            call_intrinsic!(ctx, name, $llvm_name[$val1.get_type()]($val1, $val2) -> int)
         }
     };
 }
 
-/// Invokes the [`llvm.abs`](https://llvm.org/docs/LangRef.html#llvm-abs-intrinsic) intrinsic.
+#[doc = llvm_doc!("abs")]
 ///
 /// * `src` - The value for which the absolute value is to be returned.
 /// * `is_int_min_poison` - Whether `poison` is to be returned if `src` is `INT_MIN`.
@@ -285,86 +248,48 @@ pub fn call_int_abs<'ctx>(
     debug_assert_eq!(is_int_min_poison.get_type().get_bit_width(), 1);
     debug_assert!(is_int_min_poison.is_const());
 
-    let src_type = src.get_type();
-    generate_llvm_intrinsic_fn_body!(
-        ctx,
-        name,
-        "abs",
-        BasicValueEnum::into_int_value,
-        src_type,
-        src,
-        is_int_min_poison
-    )
+    call_intrinsic!(ctx, name, "abs"[src.get_type()](src, is_int_min_poison) -> int)
 }
 
-generate_llvm_intrinsic_fn!("int", call_int_smax, "smax", a, b);
-generate_llvm_intrinsic_fn!("int", call_int_smin, "smin", a, b);
-generate_llvm_intrinsic_fn!("int", call_int_umax, "umax", a, b);
-generate_llvm_intrinsic_fn!("int", call_int_umin, "umin", a, b);
-generate_llvm_intrinsic_fn!("int", call_expect, "expect", val, expected_val);
+generate_llvm_intrinsic_fn!(int call_int_smax: "smax"(a, b));
+generate_llvm_intrinsic_fn!(int call_int_smin: "smin"(a, b));
+generate_llvm_intrinsic_fn!(int call_int_umax: "umax"(a, b));
+generate_llvm_intrinsic_fn!(int call_int_umin: "umin"(a, b));
+generate_llvm_intrinsic_fn!(int call_expect: "expect"(val, expected_val));
 
-generate_llvm_intrinsic_fn!("float", call_float_sqrt, "sqrt", val);
-generate_llvm_intrinsic_fn!("float", call_float_sin, "sin", val);
-generate_llvm_intrinsic_fn!("float", call_float_cos, "cos", val);
-generate_llvm_intrinsic_fn!("float", call_float_pow, "pow", val, power);
-generate_llvm_intrinsic_fn!("float", call_float_exp, "exp", val);
-generate_llvm_intrinsic_fn!("float", call_float_exp2, "exp2", val);
-generate_llvm_intrinsic_fn!("float", call_float_log, "log", val);
-generate_llvm_intrinsic_fn!("float", call_float_log10, "log10", val);
-generate_llvm_intrinsic_fn!("float", call_float_log2, "log2", val);
-generate_llvm_intrinsic_fn!("float", call_float_fabs, "fabs", src);
-generate_llvm_intrinsic_fn!("float", call_float_minnum, "minnum", val, power);
-generate_llvm_intrinsic_fn!("float", call_float_maxnum, "maxnum", val, power);
-generate_llvm_intrinsic_fn!("float", call_float_copysign, "copysign", mag, sgn);
-generate_llvm_intrinsic_fn!("float", call_float_floor, "floor", val);
-generate_llvm_intrinsic_fn!("float", call_float_ceil, "ceil", val);
-generate_llvm_intrinsic_fn!("float", call_float_round, "round", val);
-generate_llvm_intrinsic_fn!("float", call_float_rint, "rint", val);
+generate_llvm_intrinsic_fn!(float call_float_sqrt: "sqrt"(val));
+generate_llvm_intrinsic_fn!(float call_float_sin: "sin"(val));
+generate_llvm_intrinsic_fn!(float call_float_cos: "cos"(val));
+generate_llvm_intrinsic_fn!(float call_float_pow: "pow"(val, power));
+generate_llvm_intrinsic_fn!(float call_float_exp: "exp"(val));
+generate_llvm_intrinsic_fn!(float call_float_exp2: "exp2"(val));
+generate_llvm_intrinsic_fn!(float call_float_log: "log"(val));
+generate_llvm_intrinsic_fn!(float call_float_log10: "log10"(val));
+generate_llvm_intrinsic_fn!(float call_float_log2: "log2"(val));
+generate_llvm_intrinsic_fn!(float call_float_fabs: "fabs"(src));
+generate_llvm_intrinsic_fn!(float call_float_minnum: "minnum"(val, power));
+generate_llvm_intrinsic_fn!(float call_float_maxnum: "maxnum"(val, power));
+generate_llvm_intrinsic_fn!(float call_float_copysign: "copysign"(mag, sgn));
+generate_llvm_intrinsic_fn!(float call_float_floor: "floor"(val));
+generate_llvm_intrinsic_fn!(float call_float_ceil: "ceil"(val));
+generate_llvm_intrinsic_fn!(float call_float_round: "round"(val));
+generate_llvm_intrinsic_fn!(float call_float_rint: "rint"(val));
 
-/// Invokes the [`llvm.powi`](https://llvm.org/docs/LangRef.html#llvm-powi-intrinsic) intrinsic.
+#[doc = llvm_doc!("powi")]
 pub fn call_float_powi<'ctx>(
     ctx: &CodeGenContext<'ctx, '_>,
     val: FloatValue<'ctx>,
     power: IntValue<'ctx>,
     name: Option<&str>,
 ) -> FloatValue<'ctx> {
-    const FN_NAME: &str = "llvm.powi";
-
-    let llvm_val_t = val.get_type();
-    let llvm_power_t = power.get_type();
-
-    let intrinsic_fn = Intrinsic::find(FN_NAME)
-        .and_then(|intrinsic| {
-            intrinsic.get_declaration(&ctx.module, &[llvm_val_t.into(), llvm_power_t.into()])
-        })
-        .unwrap();
-
-    ctx.builder
-        .build_call(intrinsic_fn, &[val.into(), power.into()], name.unwrap_or_default())
-        .map(CallSiteValue::try_as_basic_value)
-        .map(|v| v.map_left(BasicValueEnum::into_float_value))
-        .map(Either::unwrap_left)
-        .unwrap()
+    call_intrinsic!(ctx, name, "powi"[val.get_type(), power.get_type()](val, power) -> float)
 }
 
-/// Invokes the [`llvm.ctpop`](https://llvm.org/docs/LangRef.html#llvm-ctpop-intrinsic) intrinsic.
+#[doc = llvm_doc!("ctpop")]
 pub fn call_int_ctpop<'ctx>(
     ctx: &CodeGenContext<'ctx, '_>,
     src: IntValue<'ctx>,
     name: Option<&str>,
 ) -> IntValue<'ctx> {
-    const FN_NAME: &str = "llvm.ctpop";
-
-    let llvm_src_t = src.get_type();
-
-    let intrinsic_fn = Intrinsic::find(FN_NAME)
-        .and_then(|intrinsic| intrinsic.get_declaration(&ctx.module, &[llvm_src_t.into()]))
-        .unwrap();
-
-    ctx.builder
-        .build_call(intrinsic_fn, &[src.into()], name.unwrap_or_default())
-        .map(CallSiteValue::try_as_basic_value)
-        .map(|v| v.map_left(BasicValueEnum::into_int_value))
-        .map(Either::unwrap_left)
-        .unwrap()
+    call_intrinsic!(ctx, name, "ctpop"[src.get_type()](src) -> int)
 }
