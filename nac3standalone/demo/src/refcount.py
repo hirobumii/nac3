b1b095323d7140a485db97fcce2418220cc0f1b8
David Mak <chmakac@connect.ust.hk>
2026-02-12 04:33:04 +0800

    Re-Enable refcount.py

--- nac3standalone/demo/src/refcount.py
+++ nac3standalone/demo/src/refcount.py
@@ -10,6 +10,11 @@ def output_int32_list(x: list[int32]):
     ...
 
 
+@extern
+def output_strln(x: str):
+    ...
+
+
 def create_list() -> list[int32]:
     return [5, 6, 7, 8]
 
@@ -23,31 +28,31 @@ def run() -> int32:
 
     output_refcount("data", data)
 
-    # data = data
-    # output_refcount("data", data)
+    data = data
+    output_refcount("data", data)
 
-    # data2 = data
-    # output_refcount("data", data)
-    # output_refcount("data2", data2)
+    data2 = data
+    output_refcount("data", data)
+    output_refcount("data2", data2)
 
-    # foo(data2)
-    # output_refcount("data", data)
-    # output_refcount("data2", data2)
+    foo(data2)
+    output_refcount("data", data)
+    output_refcount("data2", data2)
 
-    # if False:
-    #     data3 = data2
-    #     output_refcount("data", data)
-    #     output_refcount("data2", data2)
-    #     output_refcount("data3", data3)
+    if False:
+        data3 = data2
+        output_refcount("data", data)
+        output_refcount("data2", data2)
+        output_refcount("data3", data3)
 
-    # output_refcount("data", data)
-    # output_refcount("data2", data2)
+    output_refcount("data", data)
+    output_refcount("data2", data2)
 
-    # lst = create_list()
-    # output_int32_list(lst)
-    # output_refcount("lst", lst)
+    lst = create_list()
+    output_int32_list(lst)
+    output_refcount("lst", lst)
 
-    # lst_2d = [[1, 2], [3, 4]]
-    # output_refcount("lst_2d", lst_2d)
+    lst_2d = [[1, 2], [3, 4]]
+    output_refcount("lst_2d", lst_2d)
 
     return 0
