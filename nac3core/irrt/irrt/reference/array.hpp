c0eaf9a9c283abe46d09477a55c580e7d2b8be73
David Mak <chmakac@connect.ust.hk>
2026-03-02 12:28:06 +0800

    [core] irrt: Add trailing `elems` member to refcounted array

--- nac3core/irrt/irrt/reference/array.hpp
+++ nac3core/irrt/irrt/reference/array.hpp
@@ -5,9 +5,16 @@
 namespace __nac3_impl::reference {
 template<typename SizeT>
 struct Array {
-    void* data() { return static_cast<void*>(this + 1); }
+    void* data() { return static_cast<void*>(&elems); }
 
     ObjectHeader header;
     SizeT refcounted_elems;
+
+    // Since C++ doesn't have flexible array members like C, we declare the `elems` array with a size of 1.
+    // In practice, this struct will always be allocated with enough memory to hold the actual number of elements
+    // needed.
+    //
+    // https://devblogs.microsoft.com/oldnewthing/20040826-00/?p=38043
+    uint8_t elems[1];
 };
 }  // namespace __nac3_impl::reference
