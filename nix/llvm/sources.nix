445e8acfe07cd8a04bc3f3a0b0f489c190aff660
fsagbuya <fa@m-labs.ph>
2026-04-08 19:09:07 +0800

    LLVM 17 -> LLVM 18

--- nix/llvm/sources.nix
+++ nix/llvm/sources.nix
@@ -1,19 +1,19 @@
 {fetchurl}: rec {
-  version = "17.0.6";
+  version = "18.1.8";
   cmake = fetchurl {
     url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/cmake-${version}.src.tar.xz";
-    hash = "sha256-gH8GnFTcIMtHshwfasr92cZJ864BVgkEDWGCyrARQPQ=";
+    hash = "sha256-Wbre9ZLdNIk80xnUKzI6qpkLRS0FxxgP8g8jqxtB6Dc=";
   };
   llvm = fetchurl {
     url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/llvm-${version}.src.tar.xz";
-    hash = "sha256-tjgWfaE5EmyhGRe2iAIHzG6PnRy7GkjYfQF/aX73gYg=";
+    hash = "sha256-9oz5Dzabx9AVi6cNhgsMs028Fj1v8OvGz6XlFbmy4o0=";
   };
   clang = fetchurl {
     url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/clang-${version}.src.tar.xz";
-    hash = "sha256-p49minJq4dPZpxeZltl7ErkPt2q5RCpDEQuXL/etkCk=";
+    hash = "sha256-VyT+ChMIfVV5EEzt0vizvBCiEvt5oPzayY9IgOGfRRk=";
   };
   compiler-rt = fetchurl {
     url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/compiler-rt-${version}.src.tar.xz";
-    hash = "sha256-EbjQnc+SoPkcXILe+1rZ/0rPXPBzqAwxcgS6qSLRNrQ=";
+    hash = "sha256-4FTpmpySQHIGFukny1I2OrvItPHvAoa609957I/fiS8=";
   };
 }
