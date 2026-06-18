7ead64efd59e332c5179e0d160164b6951b70d09
Sebastien Bourdeauducq <sb@m-labs.hk>
2026-06-18 08:56:47 +0800

    switch to nixpkgs-weekly
    https://determinate.systems/blog/nixpkgs-cooldown/

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The third-generation ARTIQ compiler";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
 
   outputs = {
     self,
