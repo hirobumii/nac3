8c842dda22a543988487cd26fe3a2d3e93799404
Sebastien Bourdeauducq <sb@m-labs.hk>
2026-07-25 22:52:20 +0800

    Revert "llvm: always build with clang"
    This reverts commit e4e1e2cfad3a4a8b0e5b596c4e730eb8826416cb.

--- nix/llvm/default.nix
+++ nix/llvm/default.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  clangStdenv,
+  stdenv,
   wineWow64Packages,
   fetchurl,
   cmake,
@@ -23,7 +23,7 @@ in rec {
     if msys2-env == null
     then ""
     else ".exe";
-  llvm = clangStdenv.mkDerivation rec {
+  llvm = stdenv.mkDerivation rec {
     pname = "llvm-nac3";
     version = "22.1.8";
     src = fetchurl {
@@ -109,7 +109,7 @@ in rec {
       ln -s ${llvm}/bin/llvm-as${exe_suffix} $out/bin/llvm-as-irrt${exe_suffix}
     '';
   clang = wrapCCWith rec {
-    cc = clangStdenv.mkDerivation {
+    cc = stdenv.mkDerivation {
       name = "clang-nac3";
 
       dontUnpack = true;
@@ -122,7 +122,7 @@ in rec {
       passthru.isClang = true;
     };
   };
-  compiler-rt = clangStdenv.mkDerivation {
+  compiler-rt = stdenv.mkDerivation {
     name = "compiler-rt";
     dontUnpack = true;
 
